50fe4fb4a240fbacc63be4548ae949ce02fa6466
AlexxNB <iam@alexxnb.ru>
2020-11-08 21:19:56 +0300

    update version

--- rollup-malina.js
+++ rollup-malina.js
@@ -4,7 +4,7 @@ const malinajs = require('malinajs/malina.js')
 export default function malina(option = {}) {
     return {
         name: 'malina',
-        transform(code, id) {
+        async transform(code, id) {
             if(!id.endsWith('.html') &&  !id.endsWith('.xht')) return null;
             let result;
 
@@ -13,7 +13,7 @@ export default function malina(option = {}) {
                 name: id.match(/([^/]+).(html|xht)$/)[1]
             }, option);
             try {
-                result = malinajs.compile(code, opts);
+                result = await malinajs.compile(code, opts);
             } catch (e) {
                 if(e.details) console.log(e.details);
                 throw e;
